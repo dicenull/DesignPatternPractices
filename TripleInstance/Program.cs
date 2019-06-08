74a8ddbd9c4781f0df906f541554d0fa507e4b6b
dice <dicenull.dev@gmail.com>
2019-06-08 12:18:10 +0900

    三個のインスタンスを持つクラスを作成

---
+++ TripleInstance/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TripleInstance
+{
+	public class Triple
+	{
+		private static Triple[] instance = new[]
+		{
+			new Triple(0),
+			new Triple(1),
+			new Triple(2)
+		};
+
+		private readonly int id;
+
+		private Triple(int id)
+		{
+			this.id = id;
+		}
+
+		public static Triple GetInstance(int id)
+		{
+			return instance[id];
+		}
+
+		public override string ToString()
+		{
+			return $"id = {id}";
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			for(var i = 0;i < 3;i++)
+			{
+				Console.WriteLine(Triple.GetInstance(i).ToString());
+			}
+		}
+	}
+}
