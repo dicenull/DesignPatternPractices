d66a7ddce9b93cb4565abe15d420a40a11af11e1
dice <dicenull.dev@gmail.com>
2019-06-08 11:37:20 +0900

    非同期対応のTicketMakerを作成

---
+++ TicketMaker/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TicketMaker
+{
+	public class TicketMaker
+	{
+		private int ticket = 1000;
+
+		public static TicketMaker Instance { get; } = new TicketMaker();
+
+		private TicketMaker() { }
+
+		public int NextTicketNumber
+		{
+			get
+			{
+				lock(Instance)
+				{
+					Console.WriteLine(ticket);
+					return ticket++;
+				}
+			}
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var maker = TicketMaker.Instance;
+
+			Parallel.For(0, 10, i =>
+			{
+				_ = maker.NextTicketNumber;
+			});
+		}
+	}
+}
