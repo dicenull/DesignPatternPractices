da330d76c9c5811c4610526881a2ac77ced40481
s1500729 <s1500729@sendai-nct.jp>
2019-06-16 18:08:05 +0900

    mdを作るBuilderを追加

---
+++ DocumentBuilder/MarkdownBuilder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DocumentBuilder
+{
+	class MarkdownBuilder : Builder
+	{
+		private StringBuilder strBuilder = new StringBuilder();
+
+		protected override void buildTitle(string title)
+		{
+			strBuilder.AppendLine($"#{title}");
+		}
+
+		protected override void buildItems(IEnumerable<string> items)
+		{
+			foreach(var item in items)
+			{
+				strBuilder.AppendLine($"- {item}");
+			}
+		}
+
+		protected override void buildString(string str)
+		{
+			strBuilder.AppendLine($"{str}  ");
+		}
+
+		protected override void done()
+		{
+		}
+
+		public string GetResult()
+		{
+			return strBuilder.ToString();
+		}
+	}
+}
--- DocumentBuilder/Program.cs
+++ DocumentBuilder/Program.cs
@@ -6,30 +6,37 @@ namespace DocumentBuilder
 	{
 		static void Main(string[] args)
 		{
-			Console.Write("plain or html >");
+			Console.Write("plain or html or md>");
 			var str = Console.ReadLine();
 
 			var builder = str switch
 			{
 				"plain" => (Builder)new TextBuilder(),
 				"html" => (Builder)new HTMLBuilder(),
+				"md" => (Builder)new MarkdownBuilder(),
 				_ => throw new InvalidOperationException()
 			};
 
 			var director = new Director(builder);
 			director.Construct();
 
-			if(builder is TextBuilder)
+			if (builder is TextBuilder)
 			{
 				var result = (builder as TextBuilder).GetResult();
 
 				Console.WriteLine(result);
 			}
-			else if(builder is HTMLBuilder)
+			else if (builder is HTMLBuilder)
 			{
 				var filename = (builder as HTMLBuilder).GetResult();
 
 				Console.WriteLine($"{filename}が作成されました");
+			}
+			else if (builder is MarkdownBuilder)
+			{
+				var result = (builder as MarkdownBuilder).GetResult();
+
+				Console.WriteLine(result);
 			}
 
 		}
